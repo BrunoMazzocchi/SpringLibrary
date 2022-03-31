292d29e42163975b4851300307d5ebd240a190e2
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-03-30 21:46:36 -0600

    Book service

--- src/main/java/com/example/library/LibraryAPI/domain/service/BookService.java
+++ src/main/java/com/example/library/LibraryAPI/domain/service/BookService.java
@@ -1,2 +1,33 @@
-package com.example.library.LibraryAPI.domain.service;public class BookService {
+package com.example.library.LibraryAPI.domain.service;
+
+import com.example.library.LibraryAPI.domain.repository.*;
+import com.example.library.LibraryAPI.persistence.entity.*;
+import org.springframework.beans.factory.annotation.*;
+
+import java.util.*;
+
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAll(){
+        return bookRepository.getAll();
+    }
+
+    public Optional<Book> getBook(int bookId){
+        return bookRepository.getBook(bookId);
+    }
+
+    public Book save(Book book){
+        return bookRepository.save(book);
+    }
+
+    public boolean delete(int bookId){
+        return getBook(bookId).map(book -> {
+            bookRepository.delete(bookId);
+            return true;
+        }).orElse(false);
+        }
+
 }

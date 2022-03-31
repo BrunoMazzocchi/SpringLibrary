1b2050df5b5f9add4fbe79b0aaf81d79af7ed732
Bruno Mazzocchi <bemazzocchi@gmail.com>
2022-03-30 20:40:46 -0600

    Project created

---
+++ src/test/java/com/example/library/LibraryAPI/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library.LibraryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
